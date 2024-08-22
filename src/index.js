076b156ec5756aa4a3467531f0e6fe2b25213192
shubham <sd053366@gmail.com>
2024-08-22 21:32:27 +0530

    accordian app my first commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ root.render(
     <App />
   </>
 );
+
+
+// step 1: create-react-app
+// step 2: create github repo
+// step 3: $ npm install --save gh-pages
+// step 4:  "homepage": "http://gagandatt.github.io/my-app", 
+//               "scripts": {
+//                  "predeploy": "npm run build",
+//                 "deploy": "gh-pages -d build",
+//               } 
+// step 5: git init & git remote
+// step 6 : npm run deploy
